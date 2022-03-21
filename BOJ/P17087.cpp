2b7d55a04c44c7db87181286c679c0d0f9d19614
zlmrzlo <zlmrzlo@naver.com>
2022-03-21 10:23:38 +0900

    220321

--- BOJ/P17087.cpp
+++ BOJ/P17087.cpp
@@ -1,27 +1,34 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
-#include <algorithm>
 using namespace std;
 
+int GCD(int n1, int n2) {
+	if (n2 == 0) {
+		return n1;
+	}
+	else {
+		return GCD(n2, n1 % n2);
+	}
+}
+
 int main(void) {
-	// 동생의 수(a1)와 본인의 현재 위치(a2)를 입력받음
 	int n1, n2;
 	cin >> n1 >> n2;
 
-	// 각 동생의 현 위치를 저장함
-	vector<int> v1(n1);
+	int n3;
 	vector<int> v2(n1);
 	int i1 = 0;
 	while (n1--) {
-		cin >> v1[i1];
-		v2[i1] = abs(n2 - v1[i1]);
-		i1++;
+		cin >> n3;
+		v2[i1++] = abs(n2 - n3);
 	}
 
-	sort(v2.begin(), v2.end());
+	int a1 = v2[0];
+	for (int i = 1; i < v2.size(); i++) {
+		a1 = GCD(a1, v2[i]);
+	}
 
-	cout << v2[0] << endl;
+	cout << a1 << endl;
 
 	return 0;
 }
